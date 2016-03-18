66d2ec6a76e5142a6f13020b45910b4ad372d2ac
guillon <guillon@bc364b12-c674-4f59-9048-7b664a2ae5a8>
2016-03-18 15:09:26 +0000

    Correction

--- trunk/app/Config/Schema/CreationBase/createbase.sql
+++ trunk/app/Config/Schema/CreationBase/createbase.sql
@@ -8,15 +8,15 @@
 -- Insertion de valeur dans la table users
 --
 CREATE TABLE users (
-  id SERIAL NOT NULL PRIMARY KEY,
-  nom VARCHAR(50),
-  prenom VARCHAR(50),
-  username VARCHAR(50),
-  password VARCHAR(100),
-  email VARCHAR(100),
-  createdby INT,
-   created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    createdby INT,
+    created DATE,
+    modified DATE
 );
 
 INSERT INTO users(nom, prenom, username, password, email, createdby, created, modified) VALUES(
@@ -34,74 +34,74 @@ NOW()
 -- Création de la table organisations
 --
 CREATE TABLE organisations (
-  id SERIAL NOT NULL PRIMARY KEY,
-  raisonsociale VARCHAR(75),
-  telephone VARCHAR(15),
-  fax VARCHAR(15),
-  adresse TEXT,
-  email VARCHAR(75),
-  sigle VARCHAR(100),
-  siret VARCHAR(14),
-  ape VARCHAR(5),
-  logo TEXT,
-  cil INT DEFAULT NULL REFERENCES users(id),
-  created DATE,
-  modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    raisonsociale VARCHAR(75) NOT NULL,
+    telephone VARCHAR(15) NOT NULL,
+    fax VARCHAR(15),
+    adresse TEXT NOT NULL,
+    email VARCHAR(75) NOT NULL,
+    sigle VARCHAR(100),
+    siret VARCHAR(14) NOT NULL,
+    ape VARCHAR(5) NOT NULL,
+    logo TEXT,
+    cil INT DEFAULT NULL FOREIGN KEY REFERENCES users(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table de jointure Users Organisations
 --
 CREATE TABLE organisations_users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER NOT NULL REFERENCES users(id),
-  organisation_id INTEGER NOT NULL REFERENCES organisations(id),
-   created DATE,
-   modified DATE
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id),
+    organisation_id INTEGER NOT NULL FOREIGN KEY REFERENCES organisations(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table commentaires
 --
 CREATE TABLE services (
-  id              SERIAL      NOT NULL PRIMARY KEY,
-  libelle         VARCHAR(50) NOT NULL,
-  organisation_id INTEGER     NOT NULL REFERENCES organisations (id),
-  created         DATE,
-  modified        DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    libelle VARCHAR(50) NOT NULL,
+    organisation_id INTEGER NOT NULL FOREIGN KEY REFERENCES organisations(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table roles
 --
 CREATE TABLE roles (
-  id SERIAL NOT NULL PRIMARY KEY,
-  libelle VARCHAR(50),
-  organisation_id INTEGER NOT NULL REFERENCES organisations(id),
-   created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    libelle VARCHAR(50),
+    organisation_id INTEGER NOT NULL FOREIGN KEY REFERENCES organisations(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table de jointure Users Organisations
 --
 CREATE TABLE organisation_user_services (
-  id SERIAL PRIMARY KEY NOT NULL,
-  organisation_user_id INTEGER NOT NULL REFERENCES organisations_users(id) ON DELETE CASCADE,
-  service_id INTEGER NOT NULL REFERENCES services(id) ON DELETE CASCADE,
-  created DATE,
-  modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    organisation_user_id INTEGER NOT NULL FOREIGN KEY REFERENCES organisations_users(id) ON DELETE CASCADE,
+    service_id INTEGER NOT NULL FOREIGN KEY REFERENCES services(id) ON DELETE CASCADE,
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table de jointure Users Organisations
 --
 CREATE TABLE organisation_user_roles (
-  id SERIAL PRIMARY KEY NOT NULL,
-  organisation_user_id INTEGER NOT NULL REFERENCES organisations_users(id) ON DELETE CASCADE,
-  role_id INTEGER NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
-   created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    organisation_user_id INTEGER NOT NULL FOREIGN KEY REFERENCES organisations_users(id) ON DELETE CASCADE,
+    role_id INTEGER NOT NULL FOREIGN KEY REFERENCES roles(id) ON DELETE CASCADE,
+    created DATE,
+    modified DATE
 );
 
 --
@@ -109,11 +109,11 @@ CREATE TABLE organisation_user_roles (
 -- Insertion de valeur dans la table liste_droits
 --
 CREATE TABLE liste_droits (
-  id SERIAL NOT NULL PRIMARY KEY,
-  libelle VARCHAR(50),
-  value INTEGER UNIQUE,
-   created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    libelle VARCHAR(50),
+    value INTEGER UNIQUE,
+    created DATE,
+    modified DATE
 );
 
 INSERT INTO liste_droits (libelle, value) VALUES
@@ -137,11 +137,11 @@ INSERT INTO liste_droits (libelle, value) VALUES
 -- Création de la table role_droits
 --
 CREATE TABLE role_droits(
-  id SERIAL NOT NULL PRIMARY KEY,
-  role_id INTEGER NOT NULL REFERENCES roles(id),
-  liste_droit_id INTEGER NOT NULL REFERENCES liste_droits(id),
-   created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    role_id INTEGER NOT NULL FOREIGN KEY REFERENCES roles(id),
+    liste_droit_id INTEGER NOT NULL FOREIGN KEY REFERENCES liste_droits(id),
+    created DATE,
+    modified DATE
 );
 
 --
@@ -150,14 +150,14 @@ CREATE TABLE role_droits(
 --
 CREATE TABLE admins
 (
-  id serial NOT NULL,
-  user_id integer NOT NULL,
-  created date,
-  modified date,
-  CONSTRAINT admins_pkey PRIMARY KEY (id),
-  CONSTRAINT admins_user_id_fkey FOREIGN KEY (user_id)
-      REFERENCES users (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE CASCADE
+    id serial NOT NULL,
+    user_id integer NOT NULL,
+    created date,
+    modified date,
+    CONSTRAINT admins_pkey PRIMARY KEY (id),
+    CONSTRAINT admins_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
 INSERT INTO admins (user_id, created, modified) values(1, NOW(), NOW());
@@ -167,14 +167,14 @@ INSERT INTO admins (user_id, created, modified) values(1, NOW(), NOW());
 --
 CREATE TABLE fiches
 (
-  id serial NOT NULL,
-  user_id integer,
-  created date,
-  modified date,
-  form_id integer NOT NULL,
-  organisation_id integer,
-  numero character varying(200),
-  CONSTRAINT fiches_pkey PRIMARY KEY (id)
+    id serial NOT NULL,
+    user_id integer,
+    created date,
+    modified date,
+    form_id integer NOT NULL,
+    organisation_id integer,
+    numero character varying(200),
+    CONSTRAINT fiches_pkey PRIMARY KEY (id)
 );
 
 --
@@ -182,11 +182,11 @@ CREATE TABLE fiches
 -- Insertion de valeur dans la table etats
 --
 CREATE TABLE etats (
-  id SERIAL NOT NULL PRIMARY KEY,
-  libelle VARCHAR(50),
-  value INT,
-     created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    libelle VARCHAR(50),
+    value INT,
+    created DATE,
+    modified DATE
 );
 
 INSERT INTO etats (libelle, value) VALUES
@@ -202,145 +202,145 @@ INSERT INTO etats (libelle, value) VALUES
 -- Création de la table etat_fiches
 --
 CREATE TABLE etat_fiches (
-	id SERIAL NOT NULL PRIMARY KEY,
-	fiche_id INTEGER NOT NULL REFERENCES fiches(id) ON DELETE CASCADE,
-	etat_id INTEGER NOT NULL REFERENCES etats(id),
-	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-	previous_user_id INTEGER NOT NULL REFERENCES users(id),
-	previous_etat_id INTEGER DEFAULT NULL,
-	created DATE,
-	modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    fiche_id INTEGER NOT NULL FOREIGN KEY REFERENCES fiches(id) ON DELETE CASCADE,
+    etat_id INTEGER NOT NULL FOREIGN KEY REFERENCES etats(id),
+    user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id) ON DELETE CASCADE,
+    previous_user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id),
+    previous_etat_id INTEGER DEFAULT NULL,
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table files
 --
 CREATE TABLE files (
-  id SERIAL NOT NULL PRIMARY KEY,
-  nom VARCHAR(50),
-  url VARCHAR(100),
-  fiche_id INTEGER NOT NULL REFERENCES fiches(id),
-   created DATE,
-   modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    nom VARCHAR(50),
+    url VARCHAR(100),
+    fiche_id INTEGER NOT NULL FOREIGN KEY REFERENCES fiches(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table commentaires
 --
 CREATE TABLE commentaires (
-	id SERIAL NOT NULL PRIMARY KEY,
-	etat_fiches_id INTEGER NOT NULL REFERENCES etat_fiches(id) ON DELETE CASCADE,
-	content TEXT NOT NULL,
-	user_id INTEGER NOT NULL REFERENCES users(id),
-	destinataire_id INTEGER NOT NULL REFERENCES users(id),
-	created DATE,
-	modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    etat_fiches_id INTEGER NOT NULL FOREIGN KEY REFERENCES etat_fiches(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id),
+    destinataire_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table droits
 --
 CREATE TABLE droits (
-  id SERIAL NOT NULL PRIMARY KEY,
-  organisation_user_id INTEGER NOT NULL REFERENCES organisations_users(id) ON DELETE CASCADE,
-  liste_droit_id INTEGER NOT NULL REFERENCES liste_droits(id),
-  created DATE,
-  modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    organisation_user_id INTEGER NOT NULL FOREIGN KEY REFERENCES organisations_users(id) ON DELETE CASCADE,
+    liste_droit_id INTEGER NOT NULL FOREIGN KEY REFERENCES liste_droits(id),
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table historiques
 --
 CREATE TABLE historiques (
-  id SERIAL NOT NULL PRIMARY KEY,
-  content VARCHAR(300),
-  fiche_id INTEGER NOT NULL REFERENCES fiches(id) ON DELETE CASCADE,
-  created DATE,
-  modified DATE
+    id SERIAL NOT NULL PRIMARY KEY,
+    content VARCHAR(300),
+    fiche_id INTEGER NOT NULL FOREIGN KEY REFERENCES fiches(id) ON DELETE CASCADE,
+    created DATE,
+    modified DATE
 );
 
 --
 -- Création de la table champs
 --
 CREATE TABLE modifications (
-  id SERIAL  NOT NULL PRIMARY KEY,
-  fiches_id  INTEGER NOT NULL REFERENCES fiches (id) ON DELETE CASCADE,
-  modif VARCHAR(300) NOT NULL,
-  created  DATE,
-  modified DATE
+    id SERIAL  NOT NULL PRIMARY KEY,
+    fiches_id  INTEGER NOT NULL FOREIGN KEY REFERENCES fiches (id) ON DELETE CASCADE,
+    modif VARCHAR(300) NOT NULL,
+    created  DATE,
+    modified DATE
 );
 
 --
 -- Création de la table commentaires
 --
 CREATE TABLE notifications (
-	id SERIAL NOT NULL PRIMARY KEY,
-	user_id INTEGER NOT NULL REFERENCES users(id),
-	content INTEGER NOT NULL,
-	fiche_id INTEGER NOT NULL REFERENCES fiches(id),
-	vu BOOLEAN NOT NULL,
-	created DATE,
-	modified DATE,
-	afficher BOOLEAN
+    id SERIAL NOT NULL PRIMARY KEY,
+    user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id),
+    content INTEGER NOT NULL,
+    fiche_id INTEGER NOT NULL FOREIGN KEY REFERENCES fiches(id) ON DELETE CASCADE,
+    vu BOOLEAN NOT NULL,
+    created DATE,
+    modified DATE,
+    afficher BOOLEAN
 );
 
 --
 -- Création de la table fg_formulaires
 --
 CREATE TABLE fg_formulaires (
-  id SERIAL  NOT NULL PRIMARY KEY,
-  organisations_id  INTEGER NOT NULL REFERENCES organisations (id) ON DELETE CASCADE,
-  libelle VARCHAR(50) NOT NULL,
-  active BOOL NOT NULL,
-  created  DATE,
-  modified DATE,
-  description TEXT
+    id SERIAL  NOT NULL PRIMARY KEY,
+    organisations_id  INTEGER NOT NULL FOREIGN KEY REFERENCES organisations (id) ON DELETE CASCADE,
+    libelle VARCHAR(50) NOT NULL,
+    active BOOL NOT NULL,
+    created  DATE,
+    modified DATE,
+    description TEXT
 );
 
 --
 -- Création de la table champs
 --
 CREATE TABLE fg_champs (
-  id SERIAL  NOT NULL PRIMARY KEY,
-  formulaires_id  INTEGER NOT NULL REFERENCES fg_formulaires (id) ON DELETE CASCADE,
-  type VARCHAR(25) NOT NULL,
-  ligne INTEGER NOT NULL,
-  colonne INTEGER NOT NULL,
-  details TEXT NOT NULL,
-  created  DATE,
-  modified DATE
+    id SERIAL  NOT NULL PRIMARY KEY,
+    formulaires_id  INTEGER NOT NULL FOREIGN KEY REFERENCES fg_formulaires (id) ON DELETE CASCADE,
+    type VARCHAR(25) NOT NULL,
+    ligne INTEGER NOT NULL,
+    colonne INTEGER NOT NULL,
+    details TEXT NOT NULL,
+    created  DATE,
+    modified DATE
 );
 
 --
 -- Création de la table models
 --
 CREATE TABLE modeles (
-  id SERIAL  NOT NULL PRIMARY KEY,
-  formulaires_id  INTEGER NOT NULL REFERENCES fg_formulaires (id) ON DELETE CASCADE,
-  fichier VARCHAR(300) NOT NULL,
-  created  DATE,
-  modified DATE
+    id SERIAL  NOT NULL PRIMARY KEY,
+    formulaires_id  INTEGER NOT NULL FOREIGN KEY REFERENCES fg_formulaires (id) ON DELETE CASCADE,
+    fichier VARCHAR(300) NOT NULL,
+    created  DATE,
+    modified DATE
 );
 
 --
 -- Création de la table users
 --
 CREATE TABLE valeurs (
-  id SERIAL NOT NULL PRIMARY KEY,
-  fiche_id INTEGER NOT NULL REFERENCES fiches(id),
-  valeur TEXT NOT NULL,
-  created DATE,
-  modified DATE,
-  champ_name VARCHAR(100) NOT NULL
+    id SERIAL NOT NULL PRIMARY KEY,
+    fiche_id INTEGER NOT NULL FOREIGN KEY REFERENCES fiches(id),
+    valeur TEXT NOT NULL,
+    created DATE,
+    modified DATE,
+    champ_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE extraits
 (
-  id serial NOT NULL,
-  id_fiche integer,
-  data bytea,
-  CONSTRAINT pk_extraits PRIMARY KEY (id),
-  CONSTRAINT fk_extraits FOREIGN KEY (id_fiche)
-      REFERENCES fiches (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+    id_serial NOT NULL,
+    id_fiche integer,
+    data bytea,
+    CONSTRAINT pk_extraits PRIMARY KEY (id),
+    CONSTRAINT fk_extraits FOREIGN KEY (id_fiche)
+        REFERENCES fiches (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
 );
